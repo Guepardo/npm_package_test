87756ae1be91e9f4d44f3af5cf59b4fcd4964536
Allyson Maciel Guimarães <bsinet@hotmail.com>
2017-11-17 16:48:37 -0200

    ..

--- app/index.js
+++ app/index.js
@@ -4,8 +4,6 @@ import App from './Components/App'
 import store from './Store'
 import { Provider } from 'react-redux'
 
-const ChatProvider = createProvider('chat')
-
 class app extends Component {
   constructor(props) {
     super(props)
