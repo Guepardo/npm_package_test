172a8109305d69cc22e344588908441e1de7fde1
Allyson Maciel Guimarães <bsinet@hotmail.com>
2017-11-17 14:30:09 -0200

    .

---
+++ app/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Sample extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <p>This is a sample components</p> 
+      </div>
+    )
+  }
+}
+
+export default Sample
